bf53633ddfd84498763bbfe833f07e0f372e5438
blakeyrat@gmail.com <blakeyrat@gmail.com@2b3e0598-68e2-7d21-0df8-f556da106596>
2012-03-03 23:48:12 +0000

    More likely to make colorful images, performs a "MoveOrigin" between every layer.

--- trunk/Jazzimage/ColorUtils.cs
+++ trunk/Jazzimage/ColorUtils.cs
@@ -11,24 +11,31 @@ namespace Jazzimage
 	{
 		static public Color GetRandomColor()
 		{
-			if (RandomNumberProvider.GetRandDouble() < 0.5)
-			{
-				if (RandomNumberProvider.GetRandDouble() < 0.5)
-				{
-					if (RandomNumberProvider.GetRandDouble() < 0.5)
-					{
-						return (Color.White);
-					}
-					else
-					{
-						return (Color.Black);
-					}
-				}
-				int value = RandomNumberProvider.GetRandInt(256);
-				return (Color.FromArgb(value, value, value));
-			}
+			//double odds = RandomNumberProvider.GetRandDouble();
+
+			//if (odds < 0.25)
+			//{
+			//    if (odds < (0.25 * 0.25))
+			//    {
+			//        if (RandomNumberProvider.GetRandDouble() < 0.5)
+			//        {
+			//            return (Color.White);
+			//        }
+			//        else
+			//        {
+			//            return (Color.Black);
+			//        }
+			//    }
+			//    int value = RandomNumberProvider.GetRandInt(256);
+			//    return (Color.FromArgb(value, value, value));
+			//}
 
 			return (Color.FromArgb(RandomNumberProvider.GetRandInt(256), RandomNumberProvider.GetRandInt(256), RandomNumberProvider.GetRandInt(256)));
+		}
+
+		static public int GetRandomAlpha()
+		{
+			return (RandomNumberProvider.GetRandInt(127));
 		}
 
 		static public Color Combine(Color colorA, Color colorB)
@@ -41,16 +48,17 @@ namespace Jazzimage
 			{
 				return colorA;
 			}
-			
+
 			double totalAlpha = Convert.ToDouble(colorA.A) + Convert.ToDouble(colorB.A);
 			double aWeight = (Convert.ToDouble(colorA.A) / totalAlpha);
 			double bWeight = (Convert.ToDouble(colorB.A) / totalAlpha);
 
+			int a = Convert.ToInt32(totalAlpha / 2);
 			int r = Convert.ToInt32(Convert.ToDouble(colorA.R) * aWeight + Convert.ToDouble(colorB.R) * bWeight);
 			int g = Convert.ToInt32(Convert.ToDouble(colorA.G) * aWeight + Convert.ToDouble(colorB.G) * bWeight);
 			int b = Convert.ToInt32(Convert.ToDouble(colorA.B) * aWeight + Convert.ToDouble(colorB.B) * bWeight);
 
-			return Color.FromArgb(r, g, b);
+			return Color.FromArgb(a, r, g, b);
 		}
 
 		static public Color CapColor(int a, int r, int g, int b)
